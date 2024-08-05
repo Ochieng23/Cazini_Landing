6b22057a66ea8120152215dd43d3e1854aa18643
paul <ochiengpaul001@gmail.com>
2024-08-05 12:25:45 +0300

    Add redirection to custom domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export",
   images: {
     domains: ["res.cloudinary.com"],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "https://cazinilandingpage.azurewebsites.net/",
+          },
+        ],
+        destination: "https://cazini.co.ke/:path*",
+        permanent: true,
+      },
+    ];
   },
 };
 
